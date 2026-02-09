3beea3ba5db404f5222b3f28171019077fdaa380
Pablo Cabaleiro Noda <pablocabaleironoda@gmail.com>
2026-02-09 09:46:50 +0000

    fix: Add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/pagina-Reker">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
